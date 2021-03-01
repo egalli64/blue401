f4f7c1de69cc7e1b144b5d79c4cc46955d93162b
Marco <marco-frenc@hotmail.i>
2021-03-01 12:50:06 +0100

    insert_into

--- sql/migration.sql
+++ sql/migration.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `blue`.`parkings` (
 -- Table `blue`.`vehicles`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `blue`.`vehicles` (
-  `vehicle_id` INT NOT NULL,
+  `vehicle_id` INT NOT NULL auto_increment,
   `vehicle_type` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`vehicle_id`));
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `blue`.`users` (
   `credit` DECIMAL(6,2) NOT NULL,
   `vehicle_id` INT NULL,
   `license_plate` VARCHAR(45) NULL UNIQUE,
-  `password` VARCHAR(45) NOT NULL UNIQUE,
+  `password` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`user_id`),
   CONSTRAINT `vehicle_id`
     FOREIGN KEY (`vehicle_id`)
@@ -91,3 +91,26 @@ CREATE TABLE IF NOT EXISTS `blue`.`zone` (
   `zone_id` INT NOT NULL AUTO_INCREMENT,
   `zone_name` VARCHAR(45) NULL,
    PRIMARY KEY (`zone_id`));
+   
+   
+   
+INSERT INTO `blue`.`vehicles` (`vehicle_id`, `vehicle_type`) VALUES ('1', 'Car');
+INSERT INTO `blue`.`vehicles` (`vehicle_id`, `vehicle_type`) VALUES ('2', 'Bike');
+INSERT INTO `blue`.`vehicles` (`vehicle_id`, `vehicle_type`) VALUES ('3', 'Camper');
+INSERT INTO `blue`.`vehicles` (`vehicle_id`, `vehicle_type`) VALUES ('4', 'Electric_vehicle');
+
+INSERT INTO `blue`.`users` (`first_name`, `last_name`, `password`,`date_of_birth`, `email`, `credit`, `vehicle_id`, `license_plate`) VALUES ('Silvio', 'Berlusconi', 'pwd', '1936-05-05', 'silvio.berlusca@figa.it', '1000', '1', 'FI365GA');
+INSERT INTO `blue`.`users` (`first_name`, `last_name`, `password`,`date_of_birth`, `email`, `credit`, `vehicle_id`, `license_plate`) VALUES ('Roberto', 'Speranza', '1973-03-03', 'root', 'robby.spera@gov.it', '500', '2', 'GO324VER');
+INSERT INTO `blue`.`users` (`first_name`, `last_name`, `password`,`date_of_birth`, `email`, `credit`, `vehicle_id`, `license_plate`) VALUES ('Emanuele', 'Galli', '1970-04-02', 'roop','ema.gally@java.it', '300', '3', 'EGA64');
+
+INSERT INTO `blue`.`parkings` (`id_parking`, `parking_name`, `capacity_bike`, `capacity_car`, `capacity_camper`, `cost_bike`, `cost_car`, `cost_camper`, `capacity_electric_vehicle`, `cost_electric_vehicle`) VALUES ('1', 'Crocetta_Parking', '50', '100', '20', '3.50', '5.50', '10.00', '100', '2.50');
+INSERT INTO `blue`.`parkings` (`id_parking`, `parking_name`, `capacity_bike`, `capacity_car`, `capacity_camper`, `cost_bike`, `cost_car`, `cost_camper`, `capacity_electric_vehicle`, `cost_electric_vehicle`) VALUES ('2', 'San_Paolo_Parking', '100', '50', '20', '4.00', '6.50', '12.50', '90', '2.00');
+
+INSERT INTO `blue`.`zone` (`zone_name`) VALUES ('San Paolo');
+INSERT INTO `blue`.`zone` (`zone_name`) VALUES ('Crocetta');
+INSERT INTO `blue`.`zone` (`zone_name`) VALUES ('Santa Rita');
+INSERT INTO `blue`.`zone` (`zone_name`) VALUES ('Centro');
+
+INSERT INTO `blue`.`superusers` (`first_name`, `last_name`, `password`, `email`, `zone_id`, `telefono`) VALUES ('Daniele', 'Quaglione', 'Dany', 'daniele.q@libero.it', '1', '34556454');
+
+
