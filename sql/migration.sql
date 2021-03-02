b80712ece6e6527b0b4ef8e1fb2a8c7492d562c2
TwentySquidd6836 <emanuelecorsi96@gmail.com>
2021-03-02 09:36:29 +0100

    New migration

--- sql/migration.sql
+++ sql/migration.sql
@@ -1,13 +1,20 @@
-use blue;
+use blue; 
+drop table if exists parking_history;
+drop table if exists superusers;
+drop table if exists zone;
+drop table if exists parkings;
+drop table if exists users;
+drop table if exists vehicles;
 
 -- SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 -- SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 -- SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- CREATE SCHEMA IF NOT EXISTS `blue` DEFAULT CHARACTER SET utf8 ;
-USE `blue` ;
 
-drop table if exists parkings;
+-- -----------------------------------------------------
+-- Table `blue`.`parkings`
+-- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `blue`.`parkings` (
   `parking_id` INT NOT NULL AUTO_INCREMENT,
@@ -27,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `blue`.`parkings` (
 -- -----------------------------------------------------
 -- Table `blue`.`vehicles`
 -- -----------------------------------------------------
-drop table if exists vehicles;
+
 CREATE TABLE IF NOT EXISTS `blue`.`vehicles` (
   `vehicle_id` INT NOT NULL auto_increment,
   `vehicle_type` VARCHAR(45) NOT NULL,
@@ -37,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `blue`.`vehicles` (
 -- -----------------------------------------------------
 -- Table `blue`.`users`
 -- -----------------------------------------------------
-drop table if exists users;
+
 CREATE TABLE IF NOT EXISTS `blue`.`users` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
@@ -54,8 +61,36 @@ CREATE TABLE IF NOT EXISTS `blue`.`users` (
     REFERENCES `blue`.`vehicles` (`vehicle_id`)
 );
 
-drop table if exists superusers;
-CREATE TABLE IF NOT EXISTS `blue`.`superusers` (
+CREATE TABLE IF NOT EXISTS `blue`.`zone` (
+  `zone_id` INT NOT NULL AUTO_INCREMENT,
+  `zone_name` VARCHAR(45) NULL,
+   PRIMARY KEY (`zone_id`));
+
+
+-- -----------------------------------------------------
+-- Table `blue`.`parking_history`	.
+-- -----------------------------------------------------
+
+
+   CREATE TABLE IF NOT EXISTS `blue`.`parking_history` (
+  `parking_history_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `start_time` DATETIME NULL,
+  `end_time` DATETIME NULL,
+  `parking_id` INT NULL,
+  `zone_id` INT NOT NULL,
+  PRIMARY KEY (`parking_history_id`),
+  CONSTRAINT `parkhist_user_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `blue`.`users` (`user_id`),
+  CONSTRAINT `parkhist_parking_fk`
+    FOREIGN KEY (`parking_id`)
+    REFERENCES `blue`.`parkings` (`parking_id`),
+  CONSTRAINT `parkhist_zone_fk`
+    FOREIGN KEY (`zone_id`)
+    REFERENCES `blue`.`zone` (`zone_id`));
+   
+   CREATE TABLE IF NOT EXISTS `blue`.`superusers` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
@@ -64,43 +99,10 @@ CREATE TABLE IF NOT EXISTS `blue`.`superusers` (
   `zone_id` INT NOT NULL,
   `telefono` VARCHAR(45) NULL UNIQUE,
    PRIMARY KEY (`user_id`),
-     CONSTRAINT `zone_id`
+	CONSTRAINT `superuser_zone_fk`
     FOREIGN KEY (`zone_id`)
     REFERENCES `blue`.`zone` (`zone_id`)
 );
-
-
-
--- -----------------------------------------------------
--- Table `blue`.`parking_history`	.
--- -----------------------------------------------------
-drop table if exists parking_history;
-CREATE TABLE IF NOT EXISTS `blue`.`parking_history` (
-  `parking_history_id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `start_time` DATETIME NULL,
-  `end_time` DATETIME NULL,
-  `parking_id` INT NULL,
-  `zone_id` INT NOT NULL,
-  PRIMARY KEY (`parking_history_id`),
-  CONSTRAINT `user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `blue`.`users` (`user_id`),
-  CONSTRAINT `parking_id`
-    FOREIGN KEY (`parking_id`)
-    REFERENCES `blue`.`parkings` (`parking_id`),
-  CONSTRAINT `zone_id`
-    FOREIGN KEY (`zone_id`)
-    REFERENCES `blue`.`zone` (`zone_id`));
-
-
-drop table if exists zone;
-CREATE TABLE IF NOT EXISTS `blue`.`zone` (
-  `zone_id` INT NOT NULL AUTO_INCREMENT,
-  `zone_name` VARCHAR(45) NULL,
-   PRIMARY KEY (`zone_id`));
-   
-   
    
 INSERT INTO `blue`.`vehicles` (`vehicle_id`, `vehicle_type`) VALUES ('1', 'Car');
 INSERT INTO `blue`.`vehicles` (`vehicle_id`, `vehicle_type`) VALUES ('2', 'Bike');
@@ -108,16 +110,16 @@ INSERT INTO `blue`.`vehicles` (`vehicle_id`, `vehicle_type`) VALUES ('3', 'Campe
 INSERT INTO `blue`.`vehicles` (`vehicle_id`, `vehicle_type`) VALUES ('4', 'Electric_vehicle');
 
 INSERT INTO `blue`.`users` (`first_name`, `last_name`, `password`,`date_of_birth`, `email`, `credit`, `vehicle_id`, `license_plate`) VALUES ('Silvio', 'Berlusconi', 'pwd', '1936-05-05', 'silvio.berlusca@figa.it', '1000', '1', 'FI365GA');
-INSERT INTO `blue`.`users` (`first_name`, `last_name`, `password`,`date_of_birth`, `email`, `credit`, `vehicle_id`, `license_plate`) VALUES ('Roberto', 'Speranza', '1973-03-03', 'root', 'robby.spera@gov.it', '500', '2', 'GO324VER');
-INSERT INTO `blue`.`users` (`first_name`, `last_name`, `password`,`date_of_birth`, `email`, `credit`, `vehicle_id`, `license_plate`) VALUES ('Emanuele', 'Galli', '1970-04-02', 'roop','ema.gally@java.it', '300', '3', 'EGA64');
-
-INSERT INTO `blue`.`parkings` (`id_parking`, `parking_name`, `capacity_bike`, `capacity_car`, `capacity_camper`, `cost_bike`, `cost_car`, `cost_camper`, `capacity_electric_vehicle`, `cost_electric_vehicle`) VALUES ('1', 'Crocetta_Parking', '50', '100', '20', '3.50', '5.50', '10.00', '100', '2.50');
-INSERT INTO `blue`.`parkings` (`id_parking`, `parking_name`, `capacity_bike`, `capacity_car`, `capacity_camper`, `cost_bike`, `cost_car`, `cost_camper`, `capacity_electric_vehicle`, `cost_electric_vehicle`) VALUES ('2', 'San_Paolo_Parking', '100', '50', '20', '4.00', '6.50', '12.50', '90', '2.00');
+INSERT INTO `blue`.`users` (`first_name`, `last_name`, `password`,`date_of_birth`, `email`, `credit`, `vehicle_id`, `license_plate`) VALUES ('Roberto', 'Speranza','root', '1973-03-03', 'robby.spera@gov.it', '500', '2', 'GO324VER');
+INSERT INTO `blue`.`users` (`first_name`, `last_name`, `password`,`date_of_birth`, `email`, `credit`, `vehicle_id`, `license_plate`) VALUES ('Emanuele', 'Galli', 'roop', '1970-04-02', 'ema.gally@java.it', '300', '3', 'EGA64');
 
 INSERT INTO `blue`.`zone` (`zone_name`) VALUES ('San Paolo');
 INSERT INTO `blue`.`zone` (`zone_name`) VALUES ('Crocetta');
 INSERT INTO `blue`.`zone` (`zone_name`) VALUES ('Santa Rita');
 INSERT INTO `blue`.`zone` (`zone_name`) VALUES ('Centro');
+
+INSERT INTO parkings (`parking_name`, `capacity_bike`, `capacity_car`, `capacity_camper`, `zone_id`, `cost_bike`, `cost_car`, `cost_camper`, `capacity_electric_vehicle`, `cost_electric_vehicle`) VALUES ('Crocetta_Parking', '50', '100', '20', '2' ,'3.50', '5.50', '10.00', '100', '2.50');
+INSERT INTO parkings (`parking_name`, `capacity_bike`, `capacity_car`, `capacity_camper`, `zone_id`, `cost_bike`, `cost_car`, `cost_camper`, `capacity_electric_vehicle`, `cost_electric_vehicle`) VALUES ('San_Paolo_Parking', '100', '50', '20','1', '4.00', '6.50', '12.50', '90', '2.00');
 
 INSERT INTO `blue`.`superusers` (`first_name`, `last_name`, `password`, `email`, `zone_id`, `telefono`) VALUES ('Daniele', 'Quaglione', 'Dany', 'daniele.q@libero.it', '1', '34556454');
 
